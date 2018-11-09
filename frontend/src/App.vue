6e98e53d10be91241b639044a399c87aaf0a3806
KwakSeungeun <kseun0410@gmail.com>
2018-11-09 17:57:51 +0900

    Create frontend project

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>웹 시스템 설계 3조</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
