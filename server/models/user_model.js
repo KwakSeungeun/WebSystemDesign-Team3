d5fe6a2bb7c18ed4bee42a5419e885033c5b45d8
SeoByeongChan <qudcksquf@naver.com>
2018-11-15 00:58:13 +0900

    modified auth Boolean, and add email_token for email verification

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -7,13 +7,15 @@ var alarmSchema = new Schema({
   contents: String, //알림 내용
   read: Boolean // 알람 읽음 여부
 });
+
 var userSchema = new Schema({
   name: String,
   email: String,
   pw: String,
   phone: String,
   preference: String, // 선호하는 판매장소
-  auth: String, // ?
+  email_token: String, // 이메일 인증 토큰
+  auth: Boolean, // 허가 여부
   alarms: [alarmSchema] // 사용자가 진행중인 거래의 상태에 따른 알람
 });
 
