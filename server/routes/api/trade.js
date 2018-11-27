009b8bc172b9ddb94de108a37908e753093e783c
SeoByeongChan <qudcksquf@naver.com>
2018-11-27 21:08:08 +0900

    trade modified

--- server/routes/api/trade.js
+++ server/routes/api/trade.js
@@ -24,6 +24,7 @@ router.use(bodyParser.urlencoded({
     extended: false
 }));
 
+
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, path.resolve(__dirname, '../../public/Image'));
@@ -31,15 +32,16 @@ var storage = multer.diskStorage({
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + file.originalname);
     }
-})
+});
 
 var upload = multer({ storage: storage }).array('images', 5); // 개수제한 5, ******나중에 크기제한 10MB도 넣을것*****
                                                               // images 라는 key 값에 append 해서 보내줄 것(front)
 
-router.post('/info_upload', function(req, res, next) {
+router.post('/upload_trade', function(req, res, next) {
     upload(req, res, function (err) {
         if (err) {
-            console.log("error!");
+            console.log(err);
+            console.log("error! " + path.resolve(__dirname, '../../public/Image'));
             return res.status(500).send({success:"fail"}); // 실패시 옥션 업로드 자체를 막아버릴 것(front)
         }
         else {
@@ -52,12 +54,12 @@ router.post('/info_upload', function(req, res, next) {
             }
 
             var auction_info = new Auction({
-                book_id: res.body.book_id, // 나중에 수정
-                seller_id: res.body.seller_id, // 이메일로 해도 상관없을듯 하다. 구매자한테는 seller_id 만 쏙 빼서 날리면 되니
+                book_id: req.body.book_id, // 나중에 수정
+                seller_id: req.body.seller_id, // 이메일로 해도 상관없을듯 하다. 구매자한테는 seller_id 만 쏙 빼서 날리면 되니
                 img_url: tmp, // 이미지 주소, 정확히는 file name
-                tag: res.body.tag, //교수님 이름, 과목 등 태그
-                comment: res.body.comment, // 판매자가 남기고 싶은 말
-                state: res.body.state, // 책의 보존 상태
+                tag: req.body.tag, //교수님 이름, 과목 등 태그
+                comment: req.body.comment, // 판매자가 남기고 싶은 말
+                state: req.body.state, // 책의 보존 상태
                 buyers: [] // 최초 생성이니 빈 array
             });
 
@@ -80,14 +82,14 @@ router.post('/info_upload', function(req, res, next) {
 
                 }
                 else {
-                    return res.send({success:"success", files: req.files}); // file name 정보를 위하여 보내기
+                    return res.send({success:"success"});
                 }
             });
         }
     });
 });
 
-router.get('/auction_list', function(req, res, next) {
+router.get('/trade_list', function(req, res, next) {
     db.on('error', console.error);
 
     Auction.find({}, { "seller_id": 0 }, function(err, result){
@@ -100,10 +102,12 @@ router.get('/auction_list', function(req, res, next) {
     });
 });
 
-router.get('/my_auction_list/:seller_id', function(req, res, next) {
+router.get('/my_trade_list/:seller_id', function(req, res, next) {
     db.on('error', console.error);
 
-    Auction.find({"seller_id": req.params.seller_id}, { "seller_id": 0 }, function(err, result){
+    const ObjectId = mongoose.Types.ObjectId;
+
+    Auction.find({"seller_id": ObjectId(req.params.seller_id)}, { "seller_id": 0 }, function(err, result){
         if(err) {
             res.status(500).send({success: "fail"});
         }
@@ -116,7 +120,11 @@ router.get('/my_auction_list/:seller_id', function(req, res, next) {
 router.post('/delete', function(req, res, next) {
     db.on('error', console.error);
 
-    Auction.deleteOne({_id: req.body._id, seller_id: req.body.seller_id}, function(err) {
+    // login 검증은 무조건 필요
+
+    const ObjectId = mongoose.Types.ObjectId;
+
+    Auction.deleteOne({_id: ObjectId(req.body._id), seller_id: ObjectId(req.body.seller_id)}, function(err) {
         if(err) {
             res.status(500).send({success: "fail"});
         }
