36a88d0a7c004f4c12378d69d8813810a88f8366
KwakSeungeun <kseun0410@gmail.com>
2018-11-27 23:14:00 +0900

    Add book card

--- frontend/src/components/card/BookCard.vue
+++ frontend/src/components/card/BookCard.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <b-card border-variant="primary"
-            header="Primary"
-            header-bg-variant="primary"
-            header-text-variant="white"
-            align="center">
-            <p class="card-text">book card Test</p>
+        <b-card title="Card Title"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2">
+            <p class="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card's content
+            </p>
+            <b-button href="#" variant="primary">Go somewhere</b-button>
         </b-card>
     </div>
 </template>
