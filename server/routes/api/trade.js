ba3ef61cc986b30b13796b14b2752e083fc87988
SeoByeongChan <qudcksquf@naver.com>
2018-11-30 18:51:46 +0900

    modified trade

--- server/routes/api/trade.js
+++ server/routes/api/trade.js
@@ -23,7 +23,7 @@ const bodyParser = require('body-parser');
 
 const auth = require('../middleware/auth');
 
-router.use(auth); // trade 하는 모든 과정은 반드시 로그인 확인 여부가 필요하므로 middleware 를 가져다 씀
+//router.use(auth); // trade 하는 모든 과정은 반드시 로그인 확인 여부가 필요하므로 middleware 를 가져다 씀
 
 router.use(bodyParser.urlencoded({
     extended: false
@@ -43,7 +43,7 @@ var storage = multer.diskStorage({
     }
 });
 
-var upload = multer({ storage: storage, limits: { fileSize: 10 * 1024 * 1024 } }).array('images', 5); // 개수제한 5, 크기제한 10MB
+var upload = multer({ storage: storage }).array('images', 5); // 개수제한 5, 크기제한 10MB
                                                               // images 라는 key 값에 append 해서 보내줄 것(front)
 
 router.post('/upload_trade', function(req, res, next) {
@@ -82,11 +82,12 @@ router.post('/upload_trade', function(req, res, next) {
 
             trade_info.save(function (err) {
                 if (err) {
+                    console.log(err);
                     async.each(tmp, function (i, callback) {
                         fs.unlink(path.resolve(__dirname, "../../public/Image") + '/' + tmp[i], function(err) {
                             if(err) {
                                 console.log(tmp + " can't be deleted");
-                                callback(error);
+                                callback(new Error("can't deleted"));
                             }
                             else {
                                 callback(null);
