d47d85e4203adb49db2682d1955430c3de765845
antaeseon <ridgnfu@naver.com>
2018-12-05 22:26:47 +0900

    clear mysalestatus

--- frontend/src/views/MySaleStatus.vue
+++ frontend/src/views/MySaleStatus.vue
@@ -1,12 +1,71 @@
 <template>
-    <div id="my-sale-status">
-        <h1>거래 현황</h1>
+  <div class="pl-10" id="whole">
+    <b-form-radio-group
+      id="radios1"
+      v-model="selected"
+      :options="options"
+      name="radioOpenions"
+      class="wid-50"
+    ></b-form-radio-group>
+    <div class="mt-3">
     </div>
+
+    <div class="list-container">
+      <button v-for="trade in computedList" :key="trade.id" class="card-container">
+        <book-card :trade="trade"></book-card>
+      </button>
+    </div>
+  </div>
 </template>
 
+<script>
+import BookCard from "../components/card/BookCard";
+import _ from "lodash";
+
+//전체 0 1 2 3
+
+export default {
+  data() {
+    return {
+      Trades: [],
+      selected: 4,
+      options: [
+        { text: "전체보기", value: 4 },
+        { text: "장터중", value: 0 },
+        { text: "장터종료", value: 1 },
+        { text: "장터성공", value: 2 },
+        { text: "장터실패", value: 3 }
+      ]
+    };
+  },
+  components: {
+    BookCard
+  },
+  computed: {
+    computedList() {
+        if(this.selected==4){
+            return this.Trades
+        }else{
+            return _.filter(this.Trades, { 'status':this.selected });
+        }
+    }
+  },
+  created() {
+    (this.Trades = this.$store.state.trades), console.log(this.Trades);
+  }
+};
+</script>
+
+
+
+
 <style>
-#my-sale-status{
-    background: mediumslateblue;
+#my-sale-status {
+  background: mediumslateblue;
+}
+#whole {
+  margin-left: 2%;
+  margin-right: 2%;
 }
 </style>
 
