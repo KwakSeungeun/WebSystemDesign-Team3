698d58fb8290ebaceb712a3fcbd3197313b8e85b
KwakSeungeun <kseun0410@gmail.com>
2018-12-06 11:06:30 +0900

    Add untracked files

---
+++ frontend/src/views/SelectBuyer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="select-buyer">
+        <h1>구매자 선택</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'select-buyer',
+}
+</script>
+
+<style>
+#select-buyer{
+    background: green;
+    margin-left: 2%;
+    margin-right: 2%;
+}
+</style>
