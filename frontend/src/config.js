243ef2ffef3b3a1252c5f9fc7cc644a3fe2d8d29
KwakSeungeun <kseun0410@gmail.com>
2018-12-13 11:24:44 +0900

    Change server uri

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,11 @@
 // local environment
 export default {
-    'serverUri' : 'http://localhost:3000/',
-    // 'serverUri' : 'http://54.180.155.226:3000',
-    'image' : 'http://localhost:3000/Image/'
-    // 'image' : 'http://54.180.155.226/Image/'
+    // local
+    // 'serverUri' : 'http://localhost:3000/',
+    // 'image' : 'http://localhost:3000/Image/'
+    
+    // server
+    'image' : 'http://54.180.155.226/Image/',
+    'serverUri' : 'http://54.180.155.226:3000'
 
 }
--- server/config.js
+++ server/config.js
@@ -2,6 +2,6 @@
 module.exports = {
     'secret' : 'SeCrEtKeYfOrHaShInG',
     'mongodbUri' : 'mongodb://127.0.0.1:27017/WebTests',
-    'serverUrl' : 'http://localhost:3000'
-    // 'serverUrl' : 'http://54.180.155.226:3000'
+    // 'serverUrl' : 'http://localhost:3000'
+    'serverUrl' : 'http://54.180.155.226:3000'
 }
