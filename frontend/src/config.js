629cabf132acfe57de0f3fd9f34b7dd3f88f4a89
KwakSeungeun <kseun0410@gmail.com>
2018-12-13 13:15:13 +0900

    Modify incorrect server uri

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,7 +4,7 @@ export default {
     // 'image' : 'http://localhost:3000/Image/'
     
     // server
-    'image' : 'http://54.180.155.226/Image/',
+    'image' : 'http://54.180.155.226:3000/Image/',
     'serverUri' : 'http://54.180.155.226:3000'
 
 }
