dbb6cb1783e31feae06fedd711f5e9b5c3b78650
SeoByeongChan <qudcksquf@naver.com>
2018-12-13 17:26:44 +0900

    regenerate config

---
+++ server/config.js
@@ -0,0 +1,7 @@
+// local environment
+module.exports = {
+    'secret' : 'SeCrEtKeYfOrHaShInG',
+    'mongodbUri' : 'mongodb://127.0.0.1:27017/WebTests',
+    'serverUrl' : 'http://localhost:3000'
+    // 'serverUrl' : 'http://54.180.155.226:3000'
+}
