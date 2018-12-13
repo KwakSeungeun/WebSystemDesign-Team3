5c24c6bdd0f0bb7b696085a7e0c36b6cfbc6c1ba
SeoByeongChan <qudcksquf@naver.com>
2018-12-13 17:24:21 +0900

    cache delete

--- server/config.js
+++
@@ -1,7 +0,0 @@
-// local environment
-module.exports = {
-    'secret' : 'SeCrEtKeYfOrHaShInG',
-    'mongodbUri' : 'mongodb://127.0.0.1:27017/WebTests',
-    'serverUrl' : 'http://localhost:3000'
-    // 'serverUrl' : 'http://54.180.155.226:3000'
-}
